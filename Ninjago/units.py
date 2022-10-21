4a91cf02c11a5e873a26a533cd96cc99310591ea
opiter09 <92452568+opiter09@users.noreply.github.com>
2022-10-21 15:58:06 -0400

    wont overwrite the names file

--- Ninjago/units.py
+++ Ninjago/units.py
@@ -1,7 +1,6 @@
 file = open("testD.bin", "rb")
 reading = file.read()
 file.close()
-text = open("internal.txt", "at")
 
 import os
 try:
@@ -15,4 +14,3 @@ for i in range(4, 19589, 128):
     new = open("./units/" + str(count // 2).zfill(3) + "_" + reading[0xB0D8:0xBDAE].split(bytes(1))[count].decode("UTF-8") + ".bin", "wb")
     new.write(reading[i:(i + 128)])
     new.close()
-    text.write(reading[0xB0D8:0xBDAE].split(bytes(1))[count].decode("UTF-8") + "\n")
