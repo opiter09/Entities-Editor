ebf5974b25c3597a26c4b331a1aa229eb0ab6c43
opiter09 <92452568+opiter09@users.noreply.github.com>
2022-12-21 21:54:01 -0500

    added missing faction code

---
+++ testing/faction.py
@@ -0,0 +1,13 @@
+file = open("testD_F.bin", "rb").read()
+names = open("namesF.txt", "rt").read()
+import os
+try:
+    os.mkdir("factionFiles")
+except OSError as error:
+    pass
+    
+for i in range(len(names.split("\n"))):
+    newFile = open("factionFiles/" + str(i + 1).zfill(3) + "_" + names.split("\n")[i] + ".bin", "wb")
+    newFile.write(file[(4 + (64 * i)):(4 + (64 * (i + 1)))])
+    newFile.close()
+    
