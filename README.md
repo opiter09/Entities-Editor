7068c91bcf3c9a276782bb5cbf6c9331811d646d
opiter09 <92452568+opiter09@users.noreply.github.com>
2023-07-16 15:19:56 -0400

    adds patch generation

--- README.md
+++ README.md
@@ -9,11 +9,14 @@ which can be found at https://www.romhacking.net/utilities/793/. This utility co
 a license, however if you wish to see the source code for (a later version of) ndstool, it can be
 found at https://github.com/devkitPro/ndstool.
 
-With the 3.0 rewrite, running this is ridiculously simple. Simply drag and drop your ROM onto
-wrapper.exe. Then choose whether you want to edit the file, or create a new ROM (i.e. after
-you've finished editing). The only small tricky thing to watch out for is that both when
-first unpacking a ROM, and when rebuilding it, you will at one point see a dark grey, empty
-window. You must close this out to complete the process.
+With the 3.0 rewrite, running this is ridiculously simple. Simply move your ROM into the same
+folder as wrapper.exe, then drag and drop the former onto the latter. Then choose whether you want
+to edit the file, or create a new ROM (i.e. after you've finished editing). The only small tricky
+thing to watch out for is that both when first unpacking a ROM, and when rebuilding it, you will
+at one point see a dark grey, empty window. You must close this out to complete the process.
+
+Also, along with the new ROM, rebuilding conveniently generates an xdelta patch file for you,
+using the original CL version of xdelta found here: https://www.romhacking.net/utilities/928/.
 
 Finally, please note that if you choose to run this via python CL instead of the exe, the
 editor uses a much smaller font than normal for some strange reason.
--- wrapper.py
+++ wrapper.py
@@ -16,51 +16,54 @@ else:
     x == 2 # psgcompiler-friendly quit
 folder = sys.argv[1].split("\\")[-1][0:-4]
 
-result = ""
-layout = [ [ psg.Button("Edit Entities") ], [ psg.Button("Rebuild ROM") ] ]
-window = psg.Window("", layout, grab_anywhere = True)
-while True:
-    event, values = window.read()
-    # See if user wants to quit or window was closed
-    if (event == psg.WINDOW_CLOSED) or (event == "Quit"):
-        break
-    elif (event == "Edit Entities"):
-        result = "edit"
-        break
-    elif (event == "Rebuild ROM"):
-        result = "build"
-        break
-window.close()
+result = " "
+while (result != ""):
+    result = ""
+    layout = [ [ psg.Button("Edit Entities") ], [ psg.Button("Rebuild ROM") ] ]
+    window = psg.Window("", layout, grab_anywhere = True)
+    while True:
+        event, values = window.read()
+        # See if user wants to quit or window was closed
+        if (event == psg.WINDOW_CLOSED) or (event == "Quit"):
+            break
+        elif (event == "Edit Entities"):
+            result = "edit"
+            break
+        elif (event == "Rebuild ROM"):
+            result = "build"
+            break
+    window.close()
 
-if (result == "edit"):
-    if (os.path.exists(folder + "2") == False):
-        subprocess.run([ "dslazy.bat", "UNPACK", sys.argv[1] ])
-        os.rename("NDS_UNPACK", folder + "2")
-        subprocess.run([ "HackedNitroPaint.exe", folder + "2" + "/data/BP/Entities.ebp" ])
-        os.rename("testD.bin", folder + ".bin")
-    if (game == "battles"):
-        subprocess.run([ "murgaLua.exe", "EntitiesEditor.lua", folder + ".bin" ])
-    else:
-        subprocess.run([ "murgaLua.exe", "EntitiesEditor_N.lua", folder + ".bin" ])
-elif (result == "build"):
-    if (os.path.exists(folder + "2") == False):
-        psg.popup("There is no folder to build from!")
-    else:
-        layout = [[psg.Text("Please drag and drop " + folder + ".bin onto HackedNitroPaint.exe.\n\
-Press OK, then close the resulting window, then close this.")]]
-        window = psg.Window("", layout, grab_anywhere = True, font = "-size 12")
-        while True:
-            event, values = window.read()
-            if (event == psg.WINDOW_CLOSED) or (event == "Quit"):
-                break
-        window.close()
-        try:
-            os.remove(folder + "2" + "/data/BP/Entities.ebp")
-        except FileNotFoundError:
-            pass
-        os.rename("testC.bin", folder + "2" + "/data/BP/Entities.ebp")
-        os.rename(folder + "2", "NDS_UNPACK")
-        subprocess.run([ "dslazy.bat", "PACK", folder + "2" + ".nds" ])
-        os.rename("NDS_UNPACK", folder + "2")
+    if (result == "edit"):
+        if (os.path.exists(folder + "2") == False):
+            subprocess.run([ "dslazy.bat", "UNPACK", sys.argv[1] ])
+            os.rename("NDS_UNPACK", folder + "2")
+            subprocess.run([ "HackedNitroPaint.exe", folder + "2" + "/data/BP/Entities.ebp" ])
+            os.rename("testD.bin", folder + ".bin")
+        if (game == "battles"):
+            subprocess.run([ "murgaLua.exe", "EntitiesEditor.lua", folder + ".bin" ])
+        else:
+            subprocess.run([ "murgaLua.exe", "EntitiesEditor_N.lua", folder + ".bin" ])
+    elif (result == "build"):
+        if (os.path.exists(folder + "2") == False):
+            psg.popup("There is no folder to build from!")
+        else:
+            layout = [[psg.Text("Please drag and drop " + folder + ".bin onto HackedNitroPaint.exe.\n\
+Press OK if prompted, then close the grey window, then close this.")]]
+            window = psg.Window("", layout, grab_anywhere = True, font = "-size 12")
+            while True:
+                event, values = window.read()
+                if (event == psg.WINDOW_CLOSED) or (event == "Quit"):
+                    break
+            window.close()
+            try:
+                os.remove(folder + "2" + "/data/BP/Entities.ebp")
+            except FileNotFoundError:
+                pass
+            os.rename("testC.bin", folder + "2" + "/data/BP/Entities.ebp")
+            os.rename(folder + "2", "NDS_UNPACK")
+            subprocess.run([ "dslazy.bat", "PACK", folder + "2" + ".nds" ])
+            os.rename("NDS_UNPACK", folder + "2")
+            subprocess.run([ "xdelta3-3.0.11-x86_64.exe", "-e", "-s", folder + ".nds", folder + "2" + ".nds", folder + "2" + ".xdelta" ])
         
     
